e78a38b5763cbe35bc61ac3065f5ae9450d84b07
juugeebee <july2023@yahoo.fr>
2021-06-16 17:12:49 +0200

    a completer

---
+++ db_location.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# POLYMORPH v.2
+# Julie BOGOIN
+# June 2021
+
+##### DB LOCATION #####
+#######################
+
+guniq = ''
+seq = ''
+gnex = ''
+gnge = ''
+veAG = '2.1.1'
+vgAG = '2.1.1'
+hl_version = '?dataset=gnomad_r2_1'
